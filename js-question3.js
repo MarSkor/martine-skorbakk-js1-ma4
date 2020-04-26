32b83829dd39b35a32aaca119a8bd6dab68a2b3b
Martine <marty_notes@outlook.com>
2020-04-26 13:16:21 +0200

    Create js-question3.js

---
+++ js-question3.js
@@ -0,0 +1,23 @@
+async function fetchGames() {
+  try {
+    const response = await fetch("https://api.rawg.io/api/games/4200");
+    const game = await response.json();
+    createGameDetails(game);
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchGames();
+
+function createGameDetails(game) {
+  console.log(game);
+  const gameHeading = document.querySelector("h1");
+  gameHeading.innerHTML = game.name;
+
+  const gameDescription = document.querySelector(".description");
+  gameDescription.innerHTML = game.description;
+
+  const newImg = document.querySelector(".image");
+  const imgPath = game.background_image;
+  newImg.style.backgroundImage = "url(" + imgPath + ")";
+}
